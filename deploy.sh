bff29e8da62ef9daab4a9b24d870b4f55e8be3d6
Xecius <adrien.chaigne@outlook.com>
2021-01-02 19:08:37 +0100

    go

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
-docker build -t Xecius/multi-client:latest -t Xecius/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t Xecius/multi-server:latest -t Xecius/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t Xecius/multi-worker:latest -t Xecius/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t xecius/multi-client:latest -t xecius/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t xecius/multi-server:latest -t xecius/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t xecius/multi-worker:latest -t xecius/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push Xecius/multi-client:latest 
-docker push Xecius/multi-server:latest
-docker push Xecius/multi-worker:latest
+docker push xecius/multi-client:latest 
+docker push xecius/multi-server:latest
+docker push xecius/multi-worker:latest
 
-docker push Xecius/multi-client:$SHA 
-docker push Xecius/multi-server:$SHA 
-docker push Xecius/multi-worker:$SHA 
+docker push xecius/multi-client:$SHA 
+docker push xecius/multi-server:$SHA 
+docker push xecius/multi-worker:$SHA 
 
 kubectl apply -f k8s
-kubectl set image deployments/client-deployment client=Xecius/multi-client:$SHA 
-kubectl set image deployments/server-deployment server=Xecius/multi-server:$SHA 
-kubectl set image deployments/worker-deployment worker=Xecius/multi-worker:$SHA 
+kubectl set image deployments/client-deployment client=xecius/multi-client:$SHA 
+kubectl set image deployments/server-deployment server=xecius/multi-server:$SHA 
+kubectl set image deployments/worker-deployment worker=xecius/multi-worker:$SHA 
